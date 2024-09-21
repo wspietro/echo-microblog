a32b396615d6b43268c927a381efba5dd63537f5
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-09-21 12:08:00 -0300

    feat: tests, layout and erb for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 
   # Defines the root path route ("/")
   # root "articles#index"
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,27 @@
 require "test_helper"
 
+# run before every test
+
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Echo"
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "#{@base_title} | Help"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "#{@base_title} | About"
   end
 end
