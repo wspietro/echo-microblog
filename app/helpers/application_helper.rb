423a0d18a517ebdc3de2030f8743b8d3562220de
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-11-23 11:04:20 -0300

    feat: users form errors messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
-  def full_title(page_title = '')
-    base_title = 'Echo'
-  
+  def full_title(page_title = "")
+    base_title = "Echo"
+
     return base_title if page_title.empty?
-  
+
     "#{base_title} | #{page_title}"
+  end
+
+  def error_message_for(object, field)
+    if object.errors[field].any?
+      content_tag(:p, object.errors.full_messages_for(field).first, class: "input-error-message")
+    end
   end
 end
