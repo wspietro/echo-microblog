e495c5d1405f808aea4fb3312d205144d96ebc34
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-01-01 18:39:30 -0300

    feat: generate sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save { self.email = email.downcase }
+  before_validation { self.name = name.squish }
   # before_save { email.downcase! } - podemos utilizar bang method para alterar o atributo diretamente
   validates :name, { presence: true, length: { maximum: 50 } }
   validates :email, {
