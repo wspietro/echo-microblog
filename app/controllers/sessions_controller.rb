b11aa235dbeab660ef2461d022a4d3baaf978d02
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-01-12 12:17:17 -0300

    feat: session module and log in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email].downcase)
     if user && user.authenticate(session_params[:password])
+      # nos previne contra session fixation
+      # invalida sessao atual e cria uma nova
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination."
       render "new", status: :unprocessable_entity
