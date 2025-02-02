d3db78277bc107a50aeb21d664190b41352c875a
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-02-02 15:02:49 -0300

    feat: mobile styling

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,4 +1,13 @@
 document.addEventListener("turbo:load", () => {
+  let hamburguer = document.querySelector("#hamburguer")
+  hamburguer.addEventListener("click", (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    let menu = document.querySelector("#navbar-menu")
+    menu.classList.toggle("collapse")
+  })
+
   let account = document.querySelector("#account")
   account.addEventListener("click", (event) => {
     event.preventDefault()
