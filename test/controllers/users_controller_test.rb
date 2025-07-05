ae306cd57cb65b5c64dd252fcc8754622945b37f
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-07-05 11:58:52 -0300

    feat: users list index and tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @second_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
