a7e2c8d58def96102876118f7e3b709bd7997b5a
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-08-26 16:34:13 -0300

    feat: user mailer account activation preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -40,6 +40,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  host = "localhost:3000"
+
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
 
   config.action_mailer.perform_caching = false
 
