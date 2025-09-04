c02db4fd7445d10744f77ad742c4e11e7e599033
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2025-09-04 20:10:33 -0300

    feat:generalizing authenticated method and account activation controller edit action

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,17 @@
 class AccountActivationsController < ApplicationController
   def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      # activate the user
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      # handle invalid link
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,21 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email].downcase)
     if user&.authenticate(session_params[:password])
-      # nos previne contra session fixation
-      # invalida sessao atual e cria uma nova
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to forwarding_url || user
+      if user.activated?
+        log_in user
+        # nos previne contra session fixation
+        # invalida sessao atual e cria uma nova
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_to forwarding_url || user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Invalid email/password combination."
       render "new", status: :unprocessable_entity
